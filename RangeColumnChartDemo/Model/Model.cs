c64ce18e83efd12be899e159e155c5369f4598ab
VimalaThirumalaiKumarSF3739 <vimala.t@syncfusion.com>
2023-09-22 13:02:37 +0530

    Sample updated

---
+++ RangeColumnChartDemo/Model/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeColumnChartDemo
+{
+    public class Model
+    {
+        public Model(string month, double high2021, double low2021, double high2022, double low2022)
+        {
+            Month = month;
+            High2021 = high2021;
+            Low2021 = low2021;
+            High2022 = high2022;
+            Low2022 = low2022;
+        }
+
+        public string Month { get; set; }
+        public double High2021 { get; set; }
+        public double Low2021 { get; set; }
+        public double High2022 { get; set; }
+        public double Low2022 { get; set; }
+    }
+}
---
+++ RangeColumnChartDemo/ViewModel/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeColumnChartDemo
+{
+    public class ViewModel
+    {
+        List<Model> temperatureCollection;
+        public List<Model> TemperatureCollection
+        {
+            get
+            {
+                return temperatureCollection;
+            }
+            set
+            {
+                temperatureCollection = value;
+            }
+        }
+
+        public ViewModel()
+        {
+            TemperatureCollection = new List<Model>(ReadCSV());
+        }
+
+        public IEnumerable<Model> ReadCSV()
+        {
+            Assembly executingAssembly = typeof(App).GetTypeInfo().Assembly;
+            Stream inputStream = executingAssembly.GetManifestResourceStream("RangeColumnChartDemo.Resources.Raw.Rome monthly data.csv");
+
+            string? line;
+            List<string> lines = new List<string>();
+
+            using StreamReader reader = new StreamReader(inputStream);
+            while ((line = reader.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            return lines.Select(line =>
+            {
+                string[] data = line.Split(',');
+                return new Model(data[0], Convert.ToDouble(data[1]), Convert.ToDouble(data[2]), Convert.ToDouble(data[4]), Convert.ToDouble(data[5]));
+            });
+        }
+    }
+}
